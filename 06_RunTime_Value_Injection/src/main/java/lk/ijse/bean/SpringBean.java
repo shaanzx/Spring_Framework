cf17c86c4e7974e8436ae0bb3c7f64eede9dacf4
Shaanzx <anuhaslakshan07@gmail.com>
2025-01-29 11:56:24 +0530

    Runtime Value Injection Done

--- 06_RunTime_Value_Injection/src/main/java/lk/ijse/bean/SpringBean.java
+++ 06_RunTime_Value_Injection/src/main/java/lk/ijse/bean/SpringBean.java
@@ -1,10 +1,32 @@
 package lk.ijse.bean;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
-    public SpringBean(String name) {
+public class SpringBean implements InitializingBean {
+   /* @Autowired(required = false)
+    public SpringBean(@Value("Shan") String name, @Value("1") int id, @Value("true")boolean isActive) {
         System.out.println("Spring Bean Object Created");
+        System.out.println(id + " " + name+" "+isActive);
+    }
+
+    @Autowired(required = false)
+    public SpringBean(@Value("Shan") String name, @Value("1") int id) {
+        System.out.println("Spring Bean Object Created");
+        System.out.println(id + " " + name);
+    }*/
+    @Value("Shan")
+    private String name;
+
+    public SpringBean() {
+        System.out.println(name);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(name);
     }
 }
