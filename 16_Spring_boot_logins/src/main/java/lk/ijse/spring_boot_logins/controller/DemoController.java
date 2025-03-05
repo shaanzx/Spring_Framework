0ccc6bdd2748f8c657f8c5446ee6ae485dc23e02
Shaanzx <anuhaslakshan07@gmail.com>
2025-03-05 12:09:53 +0530

    Logging

--- 16_Spring_boot_logins/src/main/java/lk/ijse/spring_boot_logins/controller/DemoController.java
+++ 16_Spring_boot_logins/src/main/java/lk/ijse/spring_boot_logins/controller/DemoController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring_boot_logins.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/demo")
 public class DemoController {
 
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping
     public String logDemo(){
-        return "Hello World";
+        log.trace("This is a trace log message");
+        log.debug("This is a debug log message");
+        log.info("This is an info log message");
+        log.warn("This is a warn log message");
+        log.error("This is an error logging message");
+        return "Hello";
     }
 }
